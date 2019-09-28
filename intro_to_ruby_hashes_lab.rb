44dd566eb89c95d6fdbbf2493c1ebbf85b4a7714
Logan Smith <logsmith06@gmail.com>
2019-09-28 20:35:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,10 +26,15 @@ def monopoly_with_third_tier
 		:pieces => 4,
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
-		:two_pieces_owned => 50,
-		:three_pieces_owned => 100,
-		:four_pieces_owned => 200},
-		:names => {}
+		  :two_pieces_owned => 50,
+		  :three_pieces_owned => 100,
+		  :four_pieces_owned => 200},
+		  :names => {
+				:one => 5,
+				:two => 10,
+				:three => 15,
+				:four => 20
+			}
 	}
 	}
 
